5754bfd0754e26e24d8a646bb573986b1ff43416
Neira Ibrahimovic <119445388+NeiraIbrahimovic@users.noreply.github.com>
2026-09-21 16:12:49 -0700

    Add recovered project source, tests and documentation

---
+++ src/file/MovieDBTest.java
@@ -0,0 +1,37 @@
+package file;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MovieDBTest {
+
+	//instance of movie db to test
+	MovieDB db;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		//initialize movie db
+		this.db = new MovieDB ();
+		
+		//setup movie db
+		this.db.setUp("moviedata.txt", "movieratings.csv");
+	}
+
+	@Test
+	void testSetUp() { 
+		// The recovered fixture includes a seventh actor: Pretend Actor.
+		assertEquals(7, this.db.getActorsInfo().size());
+		assertEquals(7, this.db.getMoviesInfo().size());
+		
+		assertEquals("meryl streep", this.db.getActorsInfo().get(0).getName());
+		assertEquals(3, this.db.getActorsInfo().get(0).getMoviesCast().size());
+		assertEquals("doubt", this.db.getActorsInfo().get(0).getMoviesCast().get(0));
+		
+		assertEquals("doubt", this.db.getMoviesInfo().get(0).getName());
+		assertEquals(79, this.db.getMoviesInfo().get(0).getCriticRating());
+		assertEquals(78, this.db.getMoviesInfo().get(0).getAudienceRating());
+	}
+
+}
